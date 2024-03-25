073f7d3eee008b1f2641474cfcc760615cca6271
Peer Mohammad <peeru548@gmail.com>
2024-03-25 10:14:53 +0530

    string and Single linked list

--- ds1/string.js
+++ ds1/string.js
@@ -1,58 +1,15 @@
-
-// for(let i=0;i<n.length;i++){
-//   console.log(n[i]);
-// }
-
-//// fibanocci 
-// function fibanocci(n){
-//   let fib = [0,1];
-//   for(let i=2;i<=n;i++){
-//     fib[i] = fib[i-1]+fib[i-2]
-//   }
-//   return fib
-// }
-
-// console.log(fibanocci(3))
-
-
-/// factorial number //////
-
-// function factorial(n){
-//   let result =1;
-//   for(let i=2;i<=n;i++){
-//     result = result * i
-//   }
-//   return result
-// }
-
-// console.log(factorial(5))
-
-
-///is Prime
-
-// function isPrime(n){
-//   if(n<2){
-//     return false
-//   }
-//   for(let i=2;i<=n/2;i++){
-//     if(n%i==0){
-//       return false
-//     }
-//   }
-//   return true
-// }
-// console.log(isPrime(7));
-
-
-
-// let n = "melbin"
-
-// //////reverse String
-// let reverseN = ""
-// for(let i=n.length-1;i>=0;i--){
-//   reverseN += n[i]
-// }
-// console.log(reverseN);
+function isPrime(n){
+  if(n<2){
+    return false
+  }
+  for(let i=2;i<=n/2;i++){
+    if(n%i==0){
+      return false
+    }
+  }
+  return true
+}
+console.log(isPrime(7));
 
 
 function palindrome(str){
@@ -80,43 +37,29 @@ function capitailzation(str){
 
 // console.log(capitailzation('from'));
 
-// function concatt(str1,str2){
-//  if(str1 ===''){
-//   return str2
-//  }
-//   return str1[0]+ concatt(str1.slice(1),str2)
-// }
+function concatt(str1,str2){
+ if(str1 ===''){
+  return str2
+ }
+  return str1[0]+ concatt(str1.slice(1),str2)
+}
+
+function reverseStirng(str){
+  if(str == ''){
+    return str
+  }
+  return reverseStirng(str.slice(1))+str[0]
+}
+console.log(reverseStirng("hello"));
 
 
-// function reverse(str){
-//   if(str.length<=0){
-//     return str
-//   }
-//   return  reverse(str.slice(1))+ str[0]
-// }
-
-// console.log(concatt('akhil',"strawbeery"));
-
-// let str = "hello"
-// let val = str.split('').reverse().join('')
-// console.log(val);
-
-// function reverseStirng(str){
-//   if(str == ''){
-//     return str
-//   }
-//   return reverseStirng(str.slice(1))+str[0]
-// }
-// console.log(reverseStirng("hello"));
-
-
-// function string(string){
-//    string.replaceAll(string,'a')
+function string(string){
+   string.replaceAll(string,'a')
   
-//   return string
-// }
+  return string
+}
 
-// console.log(string("hello"));
+console.log(string("hello"));
 
 
 var reverseWords = function(s) {
